14c56a7551d643bcba6737eb20a4287589f1b80e
Melque Henrique <melquehl@gmail.com>
2021-06-23 16:38:49 -0300

    Classe produtos e associação (muitos para muitos)

---
+++ src/main/java/com/melquehenrique/cursomc/repositories/ProdutoRepository.java
@@ -0,0 +1,9 @@
+package com.melquehenrique.cursomc.repositories;
+
+import com.melquehenrique.cursomc.domain.Produto;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProdutoRepository extends JpaRepository<Produto, Integer> {
+}
