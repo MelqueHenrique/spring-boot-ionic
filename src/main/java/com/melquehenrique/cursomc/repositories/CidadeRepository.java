5c15da7120ed7a4bc64892273de06548fe318e3c
Melque Henrique <melquehl@gmail.com>
2021-06-23 18:27:30 -0300

    Add Estado e Cidade

---
+++ src/main/java/com/melquehenrique/cursomc/repositories/CidadeRepository.java
@@ -0,0 +1,10 @@
+package com.melquehenrique.cursomc.repositories;
+
+
+import com.melquehenrique.cursomc.domain.Cidade;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CidadeRepository extends JpaRepository<Cidade, Integer> {
+}
